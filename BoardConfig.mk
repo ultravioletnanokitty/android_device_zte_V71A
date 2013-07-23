e579eca5f89a6ca3a9318b11caab76098998472e
ultraviolet <ultravioletnanokitty@gmail.com>
2013-07-22 20:59:52 -0600

    initial device support

---
+++ BoardConfig.mk
@@ -0,0 +1,12 @@
+ ####### Board configuration for ZTE V71A ########
+ ### Copyright nobody because it's a fucking   ###
+ ### Makefile and it's ridiculous to copyright ###
+ ### such a document.            --ultraviolet ###
+ #################################################
+
+$(call inherit-product, device/zte/smarttab-common/BoardConfigCommon.mk)
+
+TARGET_BOOTLOADER_BOARD_NAME := V71A
+TARGET_KERNEL_CONFIG := v71_defconfig 
+
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := V71A
---
+++ cm.mk
@@ -0,0 +1,22 @@
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/zte/V71A/device_V71A.mk)
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 1280
+
+-include vendor/cm/config/common_versions.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := cm_V71A
+PRODUCT_DEVICE := V71A
+PRODUCT_MANUFACTURER := ZTE
+PRODUCT_BRAND := ZTE 
+PRODUCT_MODEL := Optik
+PRODUCT_CHARACTERISTICS := tablet
