563d2747aa1f16e1390b206db1b103cf44d3903f
ultraviolet <ultravioletnanokitty@gmail.com>
2013-07-22 21:17:59 -0600

    oops

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
  ### such a document.            --ultraviolet ###
  #################################################
 
-$(call inherit-product, device/zte/smarttab-common/BoardConfigCommon.mk)
+-include device/zte/smarttab-common/BoardConfigCommon.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := V71A
 TARGET_KERNEL_CONFIG := v71_defconfig 
