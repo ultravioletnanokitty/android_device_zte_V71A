1f1b90a09eca04a9ae21777c45a8dbdb6a97ff0f
ultraviolet <ultravioletnanokitty@gmail.com>
2013-10-09 11:21:11 -0600

    getting this motherfucker off the ground

--- cm.mk
+++ cm.mk
@@ -3,6 +3,7 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/zte/V71A/device_V71A.mk)
@@ -18,5 +19,5 @@ PRODUCT_NAME := cm_V71A
 PRODUCT_DEVICE := V71A
 PRODUCT_MANUFACTURER := ZTE
 PRODUCT_BRAND := ZTE 
-PRODUCT_MODEL := Optik
+PRODUCT_MODEL := SmartTab 7
 PRODUCT_CHARACTERISTICS := tablet
