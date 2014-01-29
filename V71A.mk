0c01dd094e2f12b622f786d37ff3de2803ed64bb
ultraviolet <ultravioletnanokitty@gmail.com>
2014-01-29 13:04:25 -0700

    re-unified gps

--- V71A.mk
+++ V71A.mk
@@ -7,7 +7,3 @@
 $(call inherit-product, device/zte/smarttab-common/device_common.mk)
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
- 
-# GPS
-PRODUCT_PACKAGES += \
-    gps.V55
